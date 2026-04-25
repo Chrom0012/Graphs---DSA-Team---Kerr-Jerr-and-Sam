8e390633a0071ea35bae5383f88ce188b684b392
Chrom0012 <samiajhonaaron@gmail.com>
2026-04-25 22:45:41 +0800

    Experiment

--- Sam's_Experiment_Code/Experiment_Tkinter.py
+++ Sam's_Experiment_Code/Experiment_Tkinter.py
@@ -12,11 +12,29 @@ Canvas.create_text(100, 85, text = "Node A", font = ("Arial", 12, "bold"))
 Canvas.create_oval(200, 30, 300, 140, fill = "blue")
 Canvas.create_text(250, 85, text = "Node B", font = ("Arial", 12, "bold"), fill = "white")
 Canvas.create_line(150, 85, 200, 85, fill = "black", width = 2)
+mark = 0
+lastx = 0
+lasty = 0
 
 def createNodeAtClick(location):
-    print(location.x, location.y)
-    Canvas.create_oval(location.x - 25, location.y - 25, location.x + 25, location.y + 25, fill = "green")
-    Canvas.create_text(location.x, location.y, text = f"{location.x}, {location.y}", font = ("Arial", 10, "bold"), fill = "white")
+    global mark, lastx, lasty
+    if mark == 0:
+        lastx = location.x
+        lasty = location.y
+        print(location.x, location.y)
+        Canvas.create_oval(location.x - 25, location.y - 25, location.x + 25, location.y + 25, fill = "green")
+        Canvas.create_text(location.x, location.y, text = f"{location.x}, {location.y}", font = ("Arial", 10, "bold"), fill = "white")
+        mark = 1
+    else:
+        print(location.x, location.y)
+        Canvas.create_oval(location.x - 25, location.y - 25, location.x + 25, location.y + 25, fill = "green")
+        Canvas.create_text(location.x, location.y, text = f"{location.x}, {location.y}", font = ("Arial", 10, "bold"), fill = "white")
+        Canvas.create_line(lastx, lasty, location.x, location.y, fill= "black", width = 2)
+        lastx = location.x
+        lasty = location.y
+
+
+
 Canvas.bind("<Button-1>", createNodeAtClick)
 
 Root.mainloop()
