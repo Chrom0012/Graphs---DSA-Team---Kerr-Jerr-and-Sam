ea2df291ed85f1e9b8221bff3aaca1daaaf3c446
Chrom0012 <samiajhonaaron@gmail.com>
2026-04-25 22:03:17 +0800

    Experiment

--- Sam's_Experiment_Code/Experiment_Tkinter.py
+++ Sam's_Experiment_Code/Experiment_Tkinter.py
@@ -3,4 +3,10 @@ import tkinter as tk
 Root = tk.Tk()
 Root.title("Example lang this")
 Root.geometry("500x500")
+
+Canvas = tk.Canvas(Root, height = 500, width = 500)
+Canvas.pack(pady=20)
+
+Canvas.create_oval(50, 30, 150, 140, fill = "red")
+Canvas.create_text(100, 85, text = "Node A", font = ("Arial", 12, "bold"))
 Root.mainloop()
