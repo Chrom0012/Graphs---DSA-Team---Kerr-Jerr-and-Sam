fcad662231165dd1ebf5e8709afc2e8e94b8b1e4
Chrom0012 <samiajhonaaron@gmail.com>
2026-04-25 22:09:51 +0800

    Experiment

--- Sam's_Experiment_Code/Experiment_Tkinter.py
+++ Sam's_Experiment_Code/Experiment_Tkinter.py
@@ -9,4 +9,7 @@ Canvas.pack(pady=20)
 
 Canvas.create_oval(50, 30, 150, 140, fill = "red")
 Canvas.create_text(100, 85, text = "Node A", font = ("Arial", 12, "bold"))
+Canvas.create_oval(200, 30, 300, 140, fill = "blue")
+Canvas.create_text(250, 85, text = "Node B", font = ("Arial", 12, "bold"), fill = "white")
+Canvas.create_line(150, 85, 200, 85, fill = "black", width = 2)
 Root.mainloop()
