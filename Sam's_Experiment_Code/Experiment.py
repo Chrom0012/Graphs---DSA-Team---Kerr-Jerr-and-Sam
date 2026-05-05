4b926b27ed3425e2a6e1cd8f88c9103ea6e43813
Chrom0012 <samiajhonaaron@gmail.com>
2026-05-05 13:19:16 +0800

    Update

--- Sam's_Experiment_Code/Experiment.py
+++ Sam's_Experiment_Code/Experiment.py
@@ -1,4 +1,3 @@
-# Example kunyare ng nodes and edges
 Graph = {
     'A' : ['B', 'C'],
     'B' : ['A', 'D', 'E'],
@@ -7,16 +6,9 @@ Graph = {
     'E' : ['B'],
     'F' : ['C']
 }
-# 'A' is the node, whereas 'B' and 'C' is mga edges na connected kay 'A'.
-# Example this ng Adjacency list. 
 print(Graph['A'])  # ['B', 'C']
 print(Graph['B'])  # ['A', 'D', 'E']
 print(Graph['C'])  # ['A', 'F']
-
-""" If I-traverse natin s'ya
-for i in Graph:
-    print(i, Graph[i])
-"""
 
 
 for i in Graph.items():
